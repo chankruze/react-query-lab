9c242149929eca8eb09855e2e311918241ad02a9
chankruze <chankruze@gmail.com>
2022-10-12 16:27:42 +0530

    add data fetching and error handling with useQuery
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/components/RQSuperHeroes.page.tsx
+++ src/components/RQSuperHeroes.page.tsx
@@ -5,6 +5,28 @@ Created: Wed Oct 05 2022 12:29:21 GMT+0530 (India Standard Time)
 Copyright (c) geekofia 2022 and beyond
 */
 
+import { useQuery } from "react-query";
+import axios from "axios";
+
 export const RQSuperHeroesPage = () => {
-  return <div className="p-4">RQSuperHeroesPage</div>;
+  // use react-query
+  const query = useQuery("super-heroes", () =>
+    axios.get(`${import.meta.env.VITE_API_URL}/superheroes`)
+  );
+
+  // get the query props
+  const { isLoading, isError, error, data, dataUpdatedAt } = query;
+
+  return (
+    <div className="p-4">
+      {isLoading && <p>fetching superheroes...</p>}
+      {isError && <pre>{JSON.stringify(error, null, 2)}</pre>}
+      {!isLoading && !isError && (
+        <p>{new Date(dataUpdatedAt).toLocaleString()}</p>
+      )}
+      {!isLoading && !isError && data?.data && (
+        <pre>{JSON.stringify(data.data, null, 2)}</pre>
+      )}
+    </div>
+  );
 };
